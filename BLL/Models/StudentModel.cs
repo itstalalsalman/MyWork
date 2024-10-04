25b4d5026e6150e7ad0315bd1e7805c4bd3c7343
itstalalsalman <mustangcreator@gmail.com>
2024-10-04 21:13:17 +0300

    Entered the BLL Class with DAL and Generated the StudentModel under Models

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+using BLL.DAL;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Student { get; set; }
+
+        public int Id => Student.Id;
+
+        public string Name => Student.Name;
+
+        public string Surname => Student.Surname;
+
+        public DateTime? BirthDate => Student.BirthDate;
+
+        public decimal? Gpa => Student.Gpa;
+
+
+
+    }
+}
